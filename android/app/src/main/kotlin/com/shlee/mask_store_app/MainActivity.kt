5d37f682f2604184f14c3e76e12b2be7dedf520a
이상현 <firstquarterlee@gmail.com>
2024-08-09 14:58:35 +0900

    init

---
+++ android/app/src/main/kotlin/com/shlee/mask_store_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shlee.mask_store_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/model/mask_store.dart
@@ -0,0 +1,14 @@
+class MaskStore {
+  final String name;
+  final String address;
+  final String distance;
+  final String remainStatus;
+
+  //생성자 생성(Named Argument Constructor로 만듬)
+  const MaskStore({
+    required this.name,
+    required this.address,
+    required this.distance,
+    required this.remainStatus,
+  });
+}
