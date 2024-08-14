6b092421608419e15349c99ac7316e4f78ac41e8
이상현 <firstquarterlee@gmail.com>
2024-08-14 16:11:38 +0900

    Feat: Provider 적용

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:mask_store_app/data/repository/mock_location_repository.dart';
 import 'package:mask_store_app/data/repository/mock_store_repository.dart';
 import 'package:mask_store_app/ui/main_screen.dart';
 import 'package:mask_store_app/ui/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,11 +21,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MainScreen(
-        viewModel: MainViewModel(
-          storeRepository: MockStoreRepository(),
-          locationRepository: MockLocationRepository(),
-        ),
+      home: ChangeNotifierProvider<MainViewModel>(
+        create: (BuildContext context) {
+          return MainViewModel(
+            storeRepository: MockStoreRepository(),
+            locationRepository: MockLocationRepository(),
+          );
+        },
+        child: const MainScreen(),
       ),
     );
   }
--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,30 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:mask_store_app/ui/component/store_item.dart';
 import 'package:mask_store_app/ui/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatelessWidget {
-  final MainViewModel viewModel;
-
-  const MainScreen({
-    super.key,
-    required this.viewModel,
-  });
+  const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: Text('마스크 재고 있는 곳: ${viewModel.stores.length}곳'),
       ),
-      body: ListenableBuilder(
-        listenable: viewModel,
-        builder: (BuildContext context, Widget? child) {
-          return ListView(
-            children: viewModel.stores
-                .map((store) => StoreItem(store: store))
-                .toList(),
-          );
-        },
+      body: ListView(
+        children: viewModel.stores
+            .map((store) => StoreItem(store: store)).toList(),
       ),
     );
   }
